71e50232361649f65e56c007d9f3df549f24198f
milabobo <milabobo@126.com>
2017-05-26 14:15:05 +0800

    add single linked list

---
+++ src/dsaaj/c3/E32SwapValue.java
@@ -0,0 +1,11 @@
+package dsaaj.c3;
+
+/**
+ * Created by yu.tian on 上午10:50.
+ * Email mailto:tianyu1@le.com.
+ */
+public class E32SwapValue {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/dsaaj/c3/MySLinkedList.java
@@ -0,0 +1,33 @@
+package dsaaj.c3;
+
+/**
+ * Created by yu.tian on 上午10:52.
+ * Email mailto:tianyu1@le.com.
+ *
+ * E3.11
+ */
+public class MySLinkedList<T> {
+    // keep only first pointer
+    SNode<T> first;
+    int size;
+
+    private static class SNode<T> {
+        T item;
+        SNode<T> next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("{");
+        SNode<T> start = first;
+        while (start != null) {
+            builder.append(start.item);
+            if (start.next != null) {
+                builder.append(",").append(" ");
+            }
+            start = start.next;
+        }
+        builder.append("}");
+        return builder.toString();
+    }
+}
